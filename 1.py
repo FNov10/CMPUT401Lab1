47a76ea9a3a390be54c3b5cc7a7da371eee8c91a
Fahad Naveed <fahadnaveed2002@gmail.com>
2023-01-13 07:47:32 -0700

    Updated script

--- 1.py
+++ 1.py
@@ -1,3 +1,6 @@
 import requests
 response = requests.get("https://google.com")
 print(response.text)
+
+gitscript = requests.get("https://raw.githubusercontent.com/FNov10/CMPUT401Lab1/main/1.py")
+print(gitscript.text)
